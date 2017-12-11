f77647194ca7849bf4f92b35cd41c35522409772
Robert Watson <spartan83@gmail.com>
2017-12-11 01:17:34 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"
     data.each do |holiday, supply|
       title = "#{holiday}"
-      array="  #{title.sub(/_/," ").capitalize}: "
+      array="  #{title.sub(/_/," ")}: "
       i=0
       while i<supply.length
         array<<supply[i]
