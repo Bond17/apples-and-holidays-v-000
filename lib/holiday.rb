1154d17dec4539a7c4c2e98e7a6ae359d9fa2917
Robert Watson <spartan83@gmail.com>
2017-12-11 01:00:31 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
       array=" #{holiday.capitalize}: "
       i=0
       while i<supply.length
-        array<<supply[i]
+        array<<supply[i]+", "
         i+=1
       end
       puts array
