b329f7b72937975feb7f31bbcead8ec94263691a
Robert Watson <spartan83@gmail.com>
2017-12-11 01:03:27 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, data|
     puts "#{season.capitalize}:"
     data.each do |holiday, supply|
-      array=" #{holiday.capitalize}: "
+      array="  #{holiday.capitalize}: "
       i=0
       while i<supply.length
         array<<supply[i]
@@ -71,7 +71,7 @@ def all_supplies_in_holidays(holiday_hash)
         array<<", "
       end
       end
-      
+
       puts array
     end
 
